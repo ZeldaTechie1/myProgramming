30d61586df78853fb797fbb6ef3a405361215a38
b-cancel <bryan.o.cancel@gmail.com>
2016-05-24 20:24:56 -0400

    added postfix notation Calculator

---
+++ C#/postFixCalculator/Assignment8/mainForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Assignment8
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputString = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.answerText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.consoleText = new System.Windows.Forms.TextBox();
+            this.anotherProblemButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputString
+            // 
+            this.inputString.Location = new System.Drawing.Point(12, 12);
+            this.inputString.Name = "inputString";
+            this.inputString.Size = new System.Drawing.Size(146, 20);
+            this.inputString.TabIndex = 0;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(173, 12);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(102, 20);
+            this.calculateButton.TabIndex = 1;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Postfix problem (see example)";
+            // 
+            // answerText
+            // 
+            this.answerText.Location = new System.Drawing.Point(291, 13);
+            this.answerText.Name = "answerText";
+            this.answerText.ReadOnly = true;
+            this.answerText.Size = new System.Drawing.Size(90, 20);
+            this.answerText.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(396, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Answer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(287, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Stack Operations";
+            // 
+            // consoleText
+            // 
+            this.consoleText.Location = new System.Drawing.Point(12, 85);
+            this.consoleText.Multiline = true;
+            this.consoleText.Name = "consoleText";
+            this.consoleText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.consoleText.Size = new System.Drawing.Size(263, 392);
+            this.consoleText.TabIndex = 6;
+            // 
+            // anotherProblemButton
+            // 
+            this.anotherProblemButton.Location = new System.Drawing.Point(12, 483);
+            this.anotherProblemButton.Name = "anotherProblemButton";
+            this.anotherProblemButton.Size = new System.Drawing.Size(103, 20);
+            this.anotherProblemButton.TabIndex = 7;
+            this.anotherProblemButton.Text = "Another Problem";
+            this.anotherProblemButton.UseVisualStyleBackColor = true;
+            this.anotherProblemButton.Click += new System.EventHandler(this.anotherProblemButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(12, 509);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(47, 22);
+            this.stopButton.TabIndex = 8;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(263, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "Seperate you operands and operators with a space";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 542);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.anotherProblemButton);
+            this.Controls.Add(this.consoleText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.answerText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.inputString);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputString;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox answerText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox consoleText;
+        private System.Windows.Forms.Button anotherProblemButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ C#/postFixCalculator/Assignment8/mainForm.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+PROGRAMMERS NAME: Bryan Cancel
+STUDENT ID: 20266067
+CLASS: CSCI 3228 ASSIGNMENT #: 8
+DATE DUE: 11/23/15 DATE TURNED IN: 11/18/15
+*/
+
+namespace Assignment8
+{
+
+    public partial class mainForm : Form
+    { 
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        List<TextBox> stackItems = new List<TextBox>();
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            myStack postfixStack = new myStack();
+            //read in the string fully from the inputString and parse
+            string[] postfixArr = inputString.Text.Split(' ');
+
+            int yPos = 457;
+
+            if (postfixArr.Length<2) 
+                MessageBox.Show("Please write in some more values into the textbox");
+            else//loop through the array
+            {
+                for (int opC = 0; opC < postfixArr.Length; opC++)
+                {
+                    //if opcode
+                    char temp = Convert.ToChar(postfixArr[opC][0]);
+                    if (temp == '+' || temp == '-' || temp == '*' || temp == '/' || temp == '%')
+                    {
+                        //if nothing to apply that opcode too give error message
+                        if (postfixStack.Count < 2) //FIX SOMETHING HERE
+                        {
+                            MessageBox.Show("You dont have enough values in the stack to apply this operator, please modify your input and try again");
+                            consoleText.Text = "";
+                            break;
+                        }
+                        else
+                        {
+                            consoleText.AppendText("Performing Operation " + temp + "\n"); //"console" output what is being done
+                            MessageBox.Show("OpCode found, will pop two number from the Stack and perform Operation!");
+
+                            double c, a = (double)postfixStack.Pop(); //pop the top two values to use them
+                            consoleText.AppendText("Popped: " + a + "\n");
+                            MessageBox.Show("Popped: " + a);
+                            stackItems.ElementAt(postfixStack.Count).Hide();
+                            stackItems.RemoveAt(postfixStack.Count);
+                            yPos += 20;//also kickback the yPos by 20 so it still keeps looking like what it is a stack
+                            
+                            double b = (double)postfixStack.Pop();
+                            consoleText.AppendText("Popped: " + b + "\n");
+                            MessageBox.Show("Popped: " + b);
+                            stackItems.ElementAt(postfixStack.Count).Hide();
+                            stackItems.RemoveAt(postfixStack.Count);
+                            yPos += 20;
+
+                            //calculate the new number
+                            if (temp == '+')
+                                c = b + a;
+                            else if (temp == '-')
+                                c = b - a;
+                            else if (temp == '*')
+                                c = b * a;
+                            else if (temp == '/')
+                                c = b / a;
+                            else
+                                c = b % a;
+                            //push the new number
+                            consoleText.AppendText("Pushed: " + c + "\n"); //"console" output the number you just pushed (aka result)
+                            MessageBox.Show("Pushed: " + c);
+                            postfixStack.Push(c);
+
+                            //also create and display a empty text box and fill it with the number being pushed (need to use a standard x value and width and height, the y value must adjust depending on the size of the stack
+                            TextBox tb = new TextBox();
+                            tb.Location = new Point(291, yPos);
+                            yPos = yPos - 20;
+                            tb.Height = 20;
+                            tb.Width = 150;
+                            tb.Text = Convert.ToString(c);
+                            tb.ReadOnly = true;
+                            this.Controls.Add(tb);
+                            stackItems.Add(tb);
+                        }
+                    }
+                    else
+                    {
+                        consoleText.AppendText("Pushed: " + postfixArr[opC] + "\n"); //"console" output the number you just pushed
+                        MessageBox.Show("Pushed: " + postfixArr[opC]);
+                        postfixStack.Push(Convert.ToDouble(postfixArr[opC]));
+
+                        //also create and display a empty text box and fill it with the number being pushed (need to use a standard x value and width and height, the y value must adjust depending on the size of the stack                        
+                        TextBox tb = new TextBox();
+                        tb.Location = new Point(291, yPos);
+                        yPos = yPos - 20;
+                        tb.Height = 20;
+                        tb.Width = 150;
+                        tb.Text = postfixArr[opC];
+                        tb.ReadOnly = true;
+                        this.Controls.Add(tb);
+                        stackItems.Add(tb);   
+                    }
+                }
+                answerText.Text = Convert.ToString(postfixStack.Peek());
+                consoleText.AppendText("Popped: Answer!\n");
+                MessageBox.Show("Popped: Answer!");
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void anotherProblemButton_Click(object sender, EventArgs e)
+        {
+            inputString.Text = "";
+            answerText.Text = "";
+            consoleText.Text = "";
+            stackItems.ElementAt(0).Hide();
+            stackItems.RemoveAt(0);
+        }
+    }
+
+    //MY OWN IMPLEMENTATION OF THE STACK - array based
+    public class myStack
+    {
+        private int count = 0;
+        private object[] stack;
+        public int Count
+        {
+            get { return count; }
+            set {; }
+        }
+
+        public object Pop()
+        {
+            object[] temp = new object[count - 1];
+            for (int i = 0; i < count - 1; i++)
+                temp[i] = stack[i];
+            object temp2 = stack[count - 1];
+            stack = temp;
+            count--;
+            return temp2;
+        }
+
+        public void Push(object obj)
+        {
+            object[] temp = new object[count + 1];
+            for (int i = 0; i < count; i++)
+                temp[i] = stack[i];
+            temp[count] = obj;
+            stack = temp;
+            count++;
+        }
+
+        public object Peek()
+        {
+            return stack[count - 1];
+        }
+    }
+}
