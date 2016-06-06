387ca3140ddb80fdcc109061805a72d602bcc46f
b-cancel <bryan.o.cancel@gmail.com>
2016-06-05 19:01:25 -0500

    going on to 'create the second activity'

--- AndroidDev/MyFirstApp/app/src/main/java/com/example/drnegativeslaptop/myfirstapp/MyActivity.java
+++ AndroidDev/MyFirstApp/app/src/main/java/com/example/drnegativeslaptop/myfirstapp/MyActivity.java
@@ -1,5 +1,6 @@
 package com.example.drnegativeslaptop.myfirstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+    //you should define the key for your intent's extra using a public constant. Its GOOD PRACTICE to define keys for intent extras using your app's package name as a prefix (so keys are unique)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +53,22 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    /** in order to be recognized by content_my.xml the method must be public, void ret, and have only view as a param*/
+    public void sendMessage(View view) {
+        // Do something in response to button
+        //content is the first param, cuz activity is a subclass of content
+        //class of the app component to which the system should deliver the intent is the second param
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+
+        //an intent can carry data types as key value pairs called extras. PUTEXTRA method takes the key name in the first param and the value in the second param
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivity(intent); //start an instance of this activity with this intent
+
     }
 }
